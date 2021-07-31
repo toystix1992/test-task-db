1baf44d3adb4ee60f15454ccdf9bb2d638f6d293
Parfionov Anton <toystix1992@gmail.com>
2021-07-31 18:46:49 +0300

    feat: add styles and make logic in slider.

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.main-slider').slick({
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        easing: "ease",
+        autoplay: false,
+        centerMode: false,
+        arrows : false,
+    });
+});
